ae55f7944232220e67b2fdc09a480df0d02460ed
MartinBitsch2 <cph-mb1112@stud.ek.dk>
2025-09-23 11:24:44 +0200

    FORV2 Tillidsspg. vedr. større investeringer

---
+++ FORV2/Data transformation FORV2.R
@@ -0,0 +1,20 @@
+#Data transformation
+
+library(readxl)
+library(stringr)
+FORV2 <- read_xlsx("FORV2.xlsx")
+
+FORVkol1 <- seq.Date(from = as.Date("2000-01-01"),
+                             to = as.Date("2025-09-30"),
+                             by = "month")
+FORVkol2 <- rep(FORV2$`Planer om køb af bil indenfor de næste 12 måneder?`, each=3)
+FORVkol3 <- rep(FORV2$`Planer om køb eller opførsel af bolig indenfor de næste 12 måneder?`, each=3)
+FORVkol4 <- rep(FORV2$`Planer om større forbedringer af eller renoveringer i hjemmet indenfor de næste 12 måneder?`, each=3)
+
+FORB.INVEST <- data.frame(Month=rev(FORVkol1), 
+                          `Planer om køb af bil indenfor de næste 12 måneder?`=FORVkol2, 
+                          `Planer om køb eller opførsel af bolig indenfor de næste 12 måneder?`=FORVkol3,
+                          `Planer om større forbedringer af eller renoveringer i hjemmet indenfor de næste 12 måneder?`=FORVkol4
+                          )
+
+
