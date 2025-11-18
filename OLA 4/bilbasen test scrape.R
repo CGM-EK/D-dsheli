5aeda3d07f2f37c3501d2b2b4110ffc29a1a8236
MartinBitsch2 <cph-mb1112@stud.ek.dk>
2025-11-18 11:28:21 +0100

    Update bilbasen test scrape.R

--- OLA 4/bilbasen test scrape.R
+++ OLA 4/bilbasen test scrape.R
@@ -11,8 +11,9 @@ facts <- c()
 locations <- c()
 
 
-url <- "https://www.bilbasen.dk/brugt/autocamper?includeengroscvr=true&includeleasing=false&sellertypes=dealer"
-page <- read_html(url)
+url <- "https://www.bilbasen.dk/brugt/autocamper?fuel=1&fuel=2&includeengroscvr=true&includeleasing=false&sellertypes=dealer"
+res <- GET(url, add_headers("User-Agent"="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36"))
+page <- read_html(res)
 cars <- page %>% html_nodes("article.Listing_listing__XwaYe")
 
 
@@ -24,31 +25,27 @@ for(page_num in 2:6){
   cars <- page %>% html_nodes("article.Listing_listing__XwaYe")
   
   for(car in cars){
-  makemodel <- car %>% 
-    html_node("h3.font-bold") %>% 
-    html_text()
-  makemodels <- c(makemodels, makemodel)
-  
-  price <- car %>% 
-    html_node("h3.font-bold.color-primary") %>%
-    html_text()
-  prices <- c(prices, price)
-  
-  fact <- car %>% 
-    html_node("li.ListingDetails_listItem___omDg") %>% 
-    html_text()
-  facts <- c(facts, fact)
-  
+    makemodel <- car %>% 
+      html_node("h3.font-bold") %>% 
+      html_text()
+    makemodels <- c(makemodels, makemodel)
+    
+    price <- car %>% 
+      html_node("h3.font-bold.color-primary") %>%
+      html_text()
+    prices <- c(prices, price)
+    
+    fact <- car %>% 
+      html_node("li.ListingDetails_listItem___omDg") %>% 
+      html_text()
+    facts <- c(facts, fact)
+    
   }
 }
 
 
 df <- data.frame(makemodels=makemodels, prices=prices, facts=facts)
 
-
-
-saveRDS(df, "C:\\Users\\marti\\Documents\\EK\\1. sem\\campersV1.rds")
-?saveRDS
 
 
 ############
@@ -75,24 +72,46 @@ for(car in cars){
 
 ########
 
-links <- cars %>% 
-  html_elements("a.Listing_link__6Z504") %>% 
-  html_attr("href")
 
+
+makemodels <- c()
+prices <- c()
 forhandler <- c()
 adresse <- c()
 cvr <- c()
+linkz <- c()
+specifikationer <- list()
+
+bo <- cars %>% 
+  html_elements("a.Listing_link__6Z504") %>% 
+  html_attr("href")
 
 
 #OBS få header på + sleep
-for(annonce in 1:30){
+for(page_num in 4:5){
+  url <- paste0("https://www.bilbasen.dk/brugt/autocamper?fuel=1&fuel=2&includeengroscvr=true&includeleasing=false&page=", page_num, "&sellertypes=dealer")
+  res <- GET(url, add_headers("User-Agent"="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36"))
+  page <- read_html(res)
+  bo <- page %>% 
+    html_elements("a.Listing_link__6Z504") %>% 
+    html_attr("href")
+
+for(i in 1:30){
   
-link2 <- read_html(links[annonce])
-
-link3 <- link2 %>% html_elements("div.bas-MuiVipSectionComponent-sectionHeader.bas-MuiSellerInfoComponent-headerStyles")
-
+  link2 <- read_html(bo[i])
+  
+  link3 <- link2 %>% html_elements("div.bas-MuiVipSectionComponent-sectionHeader.bas-MuiSellerInfoComponent-headerStyles")
+  
+  albert <- link2 %>% 
+    html_elements("h1.bas-MuiTypography-root.bas-MuiCarHeaderComponent-title.bas-MuiTypography-h2") %>% 
+    html_attr("title")
+  
+  gitte <- link2 %>% 
+    html_elements("span.bas-MuiCarPriceComponent-value") %>% 
+    html_text()
+  
   jannik <- link3 %>% 
-  html_elements("h2.bas-MuiTypography-root.bas-MuiTypography-h3") %>% 
+    html_elements("h2.bas-MuiTypography-root.bas-MuiTypography-h3") %>% 
     html_text()
   
   markus <- link2 %>% 
@@ -103,16 +122,79 @@ link3 <- link2 %>% html_elements("div.bas-MuiVipSectionComponent-sectionHeader.b
     html_elements("p.bas-MuiSellerInfoComponent-cvr") %>% 
     html_text()
   
-forhandler <- c(forhandler, jannik)
-adresse <- c(adresse, markus)
-cvr <- c(cvr, gsub("[^0-9]","", vera))
+  knud <- link2 %>% 
+    html_elements("td.bas-MuiTableCell-root.bas-MuiTableCell-body.bas-MuiTableCell-alignRight") %>% 
+    html_text()
+  
+  
+  makemodels <- c(makemodels, albert)
+  prices <- c(prices, gitte)
+  forhandler <- c(forhandler, jannik)
+  adresse <- c(adresse, markus)
+  cvr <- c(cvr, gsub("[^0-9]","", vera))
+  linkz <- c(links, bo)
+  
+  knudliste <- as.list(knud)
+  specifikationer <- append(specifikationer, knudliste)
+  
+  
+  #Sys.sleep(1)
 
 }
- 
-links 
- 
-
   
-  
+}
+
+for (i in 1:30) {
+  specifikationer[[paste0("df_", i)]] <- knud
+}
 
 
+lst <- specifikationer   # your list of vectors
+
+Find the maximum length
+max_len <- max(lengths(lst))
+
+Pad each vector with NA so all have the same length
+padded <- lapply(lst, function(x) {
+  c(x, rep(NA, max_len - length(x)))
+})
+
+Combine into a data frame
+df <- as.data.frame(padded)
+
+tdf <- as.data.frame(t(df))
+
+#colname####
+colname <- c("Modelår","1. registrering","Kilometertal","Drivmiddel",
+             "Brændstofforbrug",
+             "CO2 udledning",
+             "Euronorm",
+             "Periodisk afgift",
+             "Ydelse",
+             "Acceleration",
+             "Tophastighed",
+             "Geartype",
+             "Antal gear",
+             "Trækvægt",
+             "Farve",
+             "Nypris",
+             "Kategori",
+             "Type",
+             "Bagagerumsstørrelse",
+             "Vægt",
+             "Bredde",
+             "Længde",
+             "Højde",
+             "Lasteevne",
+             "Max. trækvægt m/bremse",
+             "Trækhjul",
+             "Cylindre",
+             "ABS-bremser",
+             "ESP",
+             "Airbags",
+             "Tankkapacitet",
+             "Døre")
+#colname####
+
+colnames(tdf) <- colname
+tdfclean <- tdf[-(26:28),-(33)]
